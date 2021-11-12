9cad0ea38aa96dd1434d0abb5508cb2353c5ce8a
Ami Q <amifel.quejado@gmail.com>
2021-11-12 16:50:11 +0800

    defer jquery, minify images

--- js/main.js
+++ js/main.js
@@ -1,42 +1,22 @@
 $(document).ready(function(){
-
 	//Navigation menu scrollTo
 	$('header nav ul li a').click(function(event){
 		event.preventDefault();
 		var section = $(this).attr('href');
 		var section_pos = $(section).position();
-
 		if(section_pos){
 			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
 		}
-		
 	});
-
 	$('.app_link').click(function(e){
 		event.preventDefault();
 		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
 	});
-
-
-
-
-
-
-
-
 	//Show & Hide menu on mobile
 	$('.burger_icon').click(function(){
 		$('header nav').toggleClass('show');
 		$('header .burger_icon').toggleClass('active');
 	});
-
-	
-
-
-
-
-
-
 	//wow.js on scroll animations initialization
 	wow = new WOW(
 	    {
@@ -46,24 +26,8 @@ $(document).ready(function(){
 		}
 	);
 	wow.init();
-
-
-
-
-
-
-
-
 	//parallax effect initialization
 	$('.hero').parallax("50%", 0.3);
-
-
-
-
-
-
-
-
 	//Nice scroll initialization
 	$("html").niceScroll({
 		scrollspeed: 50,
@@ -75,14 +39,6 @@ $(document).ready(function(){
 		cursorcolor : '#1f1f1f',
 		zindex : 999
 	});
-
-
-
-
-
-
-
-
 	//Testimonials slider initialization
 	$("#tslider").owlCarousel({
 		items : 1,
@@ -95,26 +51,17 @@ $(document).ready(function(){
 		autoPlay : true,
 		transitionStyle : "fade"
 	});
-
-
-
-
-
-
-
 	//Mailchimp subscription form initialization
 	$('#submit_form').submit(function(){
 		$('#mc_submit').attr('disabled', 'disabled');		
    		processing('icon', 'loading');
 	});
-
 	if($('#submit_form').length){
 		//Mailchim Subscription form
 		$('#submit_form').ajaxChimp({
 		    callback: chimpResponce
 		});
-	}	
-
+	}
 	//Mail chimp callback function
 	function chimpResponce(resp) {
    		if (resp.result === 'success') {   			
@@ -130,18 +77,9 @@ $(document).ready(function(){
 	    	$('#mc_submit').removeAttr('disabled', 'disabled');
 	    }
 	}
-
 	function processing(hide, show){
 			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
-
-
-
-
-
-
-
-
 	//Popup video
 	$('#play_video').click(function(e){
 		e.preventDefault();	
@@ -151,17 +89,10 @@ $(document).ready(function(){
 
 		$('.about_video').append(video_link).fadeIn(200);
 	});
-
 	$('.close_video').click(function(e){
 		e.preventDefault();	
-
 		$('.about_video').fadeOut(200,function(){
 			$('iframe', this).remove();
 		});
-
 	});
-
-
-
-
 });
